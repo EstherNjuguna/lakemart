1f305cdf546d0143e144ae5d234660050864abfb
+ <+>
2022-12-09 17:03:33 +0300

    created association and validations

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,4 +3,9 @@ class AuthenticationController < ApplicationController
     end
     def login
     end
+    private
+   
+    def create_params
+   params.permit(:name,:email,:password,:picture,:type)
+    end
 end
